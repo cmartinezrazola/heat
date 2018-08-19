0bbe93e4aa251fc80b1f9f954da6367ba7abc005
cmartinezrazola <cmartinezrazola@hotmail.com>
2018-08-19 16:31:02 +0200

    Add files via upload

---
+++ heat/__init__.py
@@ -0,0 +1,9 @@
+""" Heat transfer formulas.
+
+
+This code implements formulas for Heat Transfer.
+For reference check the book 'Heat Transfer' - Alain J.Chapman.
+The authors of this code are former students at the ETSII 
+in the Polytechnic University of Madrid
+
+"""
---
+++ heat/tables.py
@@ -0,0 +1,11 @@
+""" Tables.
+
+
+Tables mainly from Appendix 'Heat Transfer' Alain J. Chapman
+"""
+
+AIR = (
+    ("t", "cp", "rho", "mu", "nu", "k", "Pr"),
+    ("C", "kJ/(kg C)", "1.E-6 kg/(m s)", "1.E-6 m2/s", "1.E-3 W/(m C)", "-"),
+    (-50, 1.0064, 1.5819, 14.63, 9.25, 20.04, 0.735),
+)
